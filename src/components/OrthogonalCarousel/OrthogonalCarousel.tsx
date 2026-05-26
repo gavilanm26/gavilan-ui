c7e5193e9af1d265ada543a991ed4122dc196303
Mauricio Gavilan Aguiar <mgavilan@fgs.co>
2026-05-26 15:51:10 -0500

    fix: remove unused useMemo import to resolve tscompiler TS6133 error

--- src/components/OrthogonalCarousel/OrthogonalCarousel.tsx
+++ src/components/OrthogonalCarousel/OrthogonalCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { motion, useAnimationFrame } from "framer-motion";
 
 export interface OrthogonalCarouselProps {
